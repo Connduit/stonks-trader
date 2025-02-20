e9ed0e1f26ba27be341b5ac20e4a2e5ac32d0138
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-02-20 08:35:08 -0500

    Create AlgorithmEngine.py

---
+++ AlgorithmEngine.py
@@ -0,0 +1,5 @@
+"""
+This file should handle and process all algorithm tasks.
+Specifically for handling events (buy/sell orders?)
+This file should have overloaded functions like "handleBuyOrder" for each type of algorithm... or something to that nature.
+"""
