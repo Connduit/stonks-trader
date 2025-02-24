e74325f3d1dea1a4636945c8b88721989169627d
Connduit <fuxkvalve@gmail.com>
2025-02-24 18:12:30 -0500

    ema merges

--- EMA.py
+++ EMA.py
@@ -2,21 +2,6 @@
 Exponential Moving Average Indicator
 
 Formula:
-<<<<<<< HEAD
-    Period == len(Asset_Prices)
-"""
-
-from Indicator import Indicator
-import pandas as pd
-
-"""
-TODO: stuff needed to find ema
-- window size to know how much history to look at
-- period (number of days)
-"""
-class EMA(Indicator):
-    def __init__(self, df: pd.DataFrame):
-=======
 
 """
 
@@ -24,7 +9,6 @@ from Indicator import Indicator
 
 class EMA(Indicator):
     def __init__(self, df):
->>>>>>> d56e59566b6637020f090c657a8d0386faa31f0e
         super().__init__(df)
         self.df = df
 
@@ -32,13 +16,4 @@ class EMA(Indicator):
         #return super().__call__(*args, **kwds)
 
     def __call__(self, length):
-<<<<<<< HEAD
-        self.df["200_DAY_EMA"] = self.df["close"].ewm(span=length, adjust=False).mean()
-        # return self.df.tail(1)
-        #return self.df
-
-
-
-=======
         self.df[f'{length}_DAY_EMA'] = self.df['close'].ewm(span=length, adjust=False).mean() # TODO when adjust=False... data is calculated recursively
->>>>>>> d56e59566b6637020f090c657a8d0386faa31f0e
