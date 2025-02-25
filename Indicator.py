8116ae754be90e05325a47a97d35db66db2df260
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-02-25 08:13:57 -0500

    Update Indicator.py
    added comment about maybe changing Indicator to not be an ABC anymore

--- Indicator.py
+++ Indicator.py
@@ -1,5 +1,9 @@
 """
 Abstract class for indicators/studies
+TODO: maybe instead of making this an abstract base class for indicators/studies,
+      just have the individual indicators/studies be functions of the Indicator class. 
+      maybe for more complex indicators/studies we can switch back to having Indicator
+      be an abstract base class?
 """
 
 # from abc import ABC # not sure if this is actually needed for an abstract base class
