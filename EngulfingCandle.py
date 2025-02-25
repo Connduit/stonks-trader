9800265db45d45f60a31586f1d6b428324b5bcfa
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-02-25 08:34:04 -0500

    Update EngulfingCandle.py
    need to check if EngulfingCandle.py changes run

--- EngulfingCandle.py
+++ EngulfingCandle.py
@@ -4,9 +4,17 @@ at the moment, a class kind of seems overkill
 """
 
 # TODO: figure out if param type should be BarSet or if it should have already been converted into a DataFrame
-def engulfingCandle(bar_set, direction):
+# def engulfingCandle(bar_set, direction):
+# TODO: assume that this function is called from ScalperAlgorithm.updateMarketData() ?
+def engulfingCandle(minute_bars, symbol, direction):  
     # TODO: we should really only be looking at the current candle and previous candle
     # we don't really need to be passing all the bar_set data
+
+    latest_bars = minute_bars[symbol].tail(-2) # get 2 most recent bars (prev and current)
+    current_bar = latest_bars[1]
+    previous_bar = latest_bars[0]
+
+    # TODO: double check that all the code in here runs so far
 
     """
     Bullish Engulfing Candle:
@@ -22,4 +30,6 @@ def engulfingCandle(bar_set, direction):
     else:
         candle is neither bearish or bullish engulfing... return False
     """
+
+    #TODO: we already get minute bars from ScalperAlgorithm.updateMarketData so maybe we can reuse that code here
     pass
